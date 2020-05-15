0981801e5e8d7e07b38d33c3fcb45d83599f27cc
Tommy Werner <wtwerner@gmail.com>
2020-05-15 04:53:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads = {
-    :pieces => 4},
-    {:names => {}}
-  }
+  monopoly = {:railroads => {:pieces => 4}}
 end
 
 def monopoly_with_fourth_tier
