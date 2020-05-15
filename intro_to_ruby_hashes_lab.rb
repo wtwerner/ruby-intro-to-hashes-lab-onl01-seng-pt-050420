828d49040d7d3c5a3f9c86978eac8891cc1dcf48
Tommy Werner <wtwerner@gmail.com>
2020-05-15 05:12:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
     :pieces => 4, 
     :names => {
       :reading_railroad => {
-        
+        "mortgage_value" => "$100"
       },
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
